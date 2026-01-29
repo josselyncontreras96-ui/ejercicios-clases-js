f2d3aba5c86aefd3032c195cb08a0032d81a586a
Josselyn Contreras <josselyn.contreras96@gmail.com>
2026-01-29 15:21:52 +0100

    agregos mas ejercicios

---
+++ Clase-03/js/scripts.js
@@ -0,0 +1,62 @@
+// ejercicio 01 contador simple       ====================================================================
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// ejercicio 02 - validar palabra usando while y prompt  ============================================
+
+//let palabra = prompt("Ingresa la palabra correcta:");
+
+//while (palabra !== "html") {
+  //palabra = prompt("Incorrecto. Ingresa la palabra 'html'");
+//}
+
+//console.log("¡Correcto! Ingresaste:", palabra);
+
+
+// ejercicio 03 menu simple usando do..shile y promt ============================================
+
+/* let opcion;
+
+// do {
+ //  opcion = prompt(
+   //  "Menú:\n" +
+  //   "1 - Saludar\n" +
+   //  "2 - Salir"
+  // );
+
+  // if (opcion === "1") {
+   //  console.log("¡Hola!");
+ //  } else if (opcion === "2") {
+    // console.log("Saliendo del programa...");
+  // } else {
+  //   console.log("Opción inválida");
+  // }
+
+// } while (opcion !== "2");
+
+// ejercicio 04 contador inverso   ==============================================================
+// El for se compone de 3 partes:
+// 1) Inicialización: se crea la variable y se le da un valor inicial
+// 2) Condición: mientras esto sea verdadero, el ciclo continúa
+// 3) Actualización: se ejecuta después de cada vuelta del ciclo
+
+for (let i = 20; i >= 0; i--) {      // i empieza en 20
+  // Mientras i sea mayor o igual a 0, el ciclo sigue
+  // En cada vuelta, i disminuye en 1
+
+console.log(i); // Muestra el valor actual de i en la consola
+}
+
+// ejercicio 05 
+/*
+El bucle que me resultó más fácil fue el for, porque tiene todo en una sola línea
+(inicio, condición y aumento), lo que hace más claro cuántas veces se va a repetir.
+
+Usaría for cuando sé exactamente cuántas veces quiero repetir algo,
+por ejemplo para contar del 1 al 10 o del 20 al 0.
+
+Usaría while cuando no sé cuántas veces se va a repetir,
+por ejemplo cuando pido datos con prompt hasta que el usuario ingrese algo correcto.
+*/
